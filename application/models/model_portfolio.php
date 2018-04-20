dccabda497aa5756330fef28c4b44e28c8003d36
kda23 <kda.23@mail.com>
2018-04-21 02:46:39 +0300

    Deleted logic of function

--- application/models/model_portfolio.php
+++ application/models/model_portfolio.php
@@ -3,20 +3,7 @@ class Model_Portfolio extends Model
 {
 	public function get_data()
 	{	
-		return array(
-			
-			array(
-				'Year' => '2012',
-				'Site' => 'http://exemple.com',
-				'Description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. компанией "CАН ИнБев".'
-			),
-			array(
-				'Year' => '2012',
-				'Site' => 'http://Mobile.ru',
-				'Description' => 'ipsum dolor sit amet, consectetur adipisicing elit, sed do'
-			),
-			// todo
-		);
+
 	}
 }
 ?>
